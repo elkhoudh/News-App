f72b9ab95b820b4c220f7d2a336b7813f75d26c9
Hamza Elkhoudiri <elkhoudh@gmail.com>
2018-12-14 19:35:28 -0500

    Responsive

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
 
 const styles = theme => ({
   root: {
@@ -21,7 +25,7 @@ const styles = theme => ({
 class App extends Component {
   state = {
     searchTerm: '',
-    isSubmitted: '',
+    results: 20,
     data: []
   }
 
@@ -32,7 +36,7 @@ class App extends Component {
   }
 
   handleChange = e => {
-    this.setState({searchTerm: e.target.value})
+    this.setState({[e.target.name]: e.target.value})
   }
   
   fetchData = (e) => {
@@ -40,7 +44,7 @@ class App extends Component {
     if(this.state.searchTerm.length === 0){
       alert('Please Enter Search Term')
     } else {
-      fetch(`https://newsapi.org/v2/everything?q=${this.state.searchTerm}&apiKey=8ae4612a456a4220952d4dbd49878716`)
+      fetch(`https://newsapi.org/v2/everything?q=${this.state.searchTerm}&pageSize=${this.state.results}&apiKey=8ae4612a456a4220952d4dbd49878716`)
       .then(res => res.json())
       .then(data => this.setState({data}))
     }
@@ -50,7 +54,7 @@ class App extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div class="app">
+      <div className="app">
         <NavBar />   
         <div className={classes.root}>
         <Grid container spacing={24}>
@@ -58,12 +62,29 @@ class App extends Component {
           <Paper className={classes.paper}>
           <form onSubmit={this.fetchData}>
           <input onChange={this.handleChange}  value={this.state.searchTerm} name="searchTerm" id="searchTerm" type="text" placeholder="Search Here..." required/>
+          
+          <InputLabel htmlFor="results">Results</InputLabel>
+          <Select
+            value={this.state.results}
+            onChange={this.handleChange}
+            inputProps={{
+              name: 'results',
+              id: 'results',
+            }}
+          >
+          <MenuItem value={20}>
+              <em>20</em>
+            </MenuItem>
+            <MenuItem value={50}>50</MenuItem>
+            <MenuItem value={75}>75</MenuItem>
+            <MenuItem value={100}>100</MenuItem>
+          </Select>
           <button type="submit" onClick={this.fetchData}>Submit</button>
           </form>
           </Paper>
         </Grid>
-      {this.state.data.length === 0 ? console.log('THIS SHIT IS EMPTY') : this.state.data.articles.map(article => 
-        <Grid item xs={12} sm={6} md={3}>
+      {this.state.data.length === 0 ? '' : this.state.data.articles.map(article => 
+        <Grid key={article.url} item xs={12} sm={6} md={3}>
           <Card 
             key={article.url} 
             author={article.author} 
